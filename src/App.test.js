f221a2873700a807198acf83da6258caf2ca4c55
ADamian17 <adonisdesuvikdo@gmail.com>
2022-03-13 09:51:50 -0700

    all test for sign up are passing

--- src/App.test.js
+++ src/App.test.js
@@ -30,91 +30,89 @@ const typeIntoForm = ({email, password, confirmPassword }) => {
   }
 }
 
-test('input should be initially epmty', () => {
-  const email = screen.getByTestId('form-input-email');
-  const password = screen.getByTestId('form-input-password');
-  const confirmPassword = screen.getByTestId('form-input-confirm-password');
-  
-  expect(email.value).toBe('')
-  expect(password.value).toBe('')
-  expect(confirmPassword.value).toBe('')
-});
+const findSubmitBtnAndClick = (btnTargetId) => userEvents.click(screen.getByTestId(btnTargetId))
 
-test('should be able to type an email', () => {
-  const {emailEl} = typeIntoForm({email: 'test@gmail.com'})
-  expect(emailEl.value).toBe('test@gmail.com')
-});
+describe('testing app component', () => {
 
-test('should be able to type a password', () => {
-  const {passwordEl} = typeIntoForm({password: '1234'})
-  expect(passwordEl.value).toBe('1234')
-});
-
-test('should be able to type a confirm password', () => {
-  const { confirmPasswordEl } = typeIntoForm({confirmPassword: '1234'});
-  expect(confirmPasswordEl.value).toBe('1234')
-});
-
-test('should not show email error message', () => {
-  const emailError = screen.queryByTestId('form-input-email-error');
-  expect(emailError).not.toBeInTheDocument();
-});
-
-test('should show email error message on invalid email', () => {
-  typeIntoForm({email: 'testgmail.com'})
-  
-  const submitBtn = screen.getByTestId('form-submit-btn');
-  userEvents.click(submitBtn)
-  
-  const emailError = screen.queryByTestId('form-input-email-error');
-  expect(emailError).toBeInTheDocument();
-});
-
-test('should not show password error message', () => {
-  const passwordError = screen.queryByTestId('form-input-password-error');
-  expect(passwordError).not.toBeInTheDocument();
-});
-
-test('should show password error message on weak password', () => {
-  typeIntoForm({password: '1234'});
-  
-  const submitBtn = screen.getByTestId('form-submit-btn');
-  userEvents.click(submitBtn)
-  
-  const passwordError = screen.queryByTestId('form-input-password-error');
-  expect(passwordError).toBeInTheDocument();
-});
-
-test('should not show confirm password error message', () => {
-  const confrimPasswordError = screen.queryByTestId('form-input-confirm-password-error');
-  expect(confrimPasswordError).not.toBeInTheDocument();
-});
-
-test('should show confirm password error message if password does not match', () => {
-  const {passwordEl, confirmPasswordEl} = typeIntoForm({
-    password: '1234', 
-    confirmPassword: '1232'
+  test('input should be initially epmty', () => {
+    const email = screen.getByTestId('form-input-email');
+    const password = screen.getByTestId('form-input-password');
+    const confirmPassword = screen.getByTestId('form-input-confirm-password');
+    
+    expect(email.value).toBe('')
+    expect(password.value).toBe('')
+    expect(confirmPassword.value).toBe('')
   });
 
-  expect(passwordEl.value !== confirmPasswordEl.value).toBeTruthy();
-
-  const submitBtn = screen.getByTestId('form-submit-btn');
-  userEvents.click(submitBtn)
-
-  const confrimPasswordError = screen.queryByTestId('form-input-confirm-password-error');
-  expect(confrimPasswordError).toBeInTheDocument();
-});
-
-test('should not show error messages if data is valid', () => {
-  typeIntoForm({
-    email: 'adonis@gmail.com',
-    password: 'XUG0gxf0ztx!rea3vhe',
-    confirmPassword: 'XUG0gxf0ztx!rea3vhe'
+  test('should be able to type an email', () => {
+    const {emailEl} = typeIntoForm({email: 'test@gmail.com'})
+    expect(emailEl.value).toBe('test@gmail.com')
   });
 
-  const passwordError = screen.queryByTestId('form-input-password-error');
-  expect(passwordError).not.toBeInTheDocument();
+  test('should be able to type a password', () => {
+    const {passwordEl} = typeIntoForm({password: '1234'})
+    expect(passwordEl.value).toBe('1234')
+  });
 
-  const confrimPasswordError = screen.queryByTestId('form-input-confirm-password-error');
-  expect(confrimPasswordError).not.toBeInTheDocument();
+  test('should be able to type a confirm password', () => {
+    const { confirmPasswordEl } = typeIntoForm({confirmPassword: '1234'});
+    expect(confirmPasswordEl.value).toBe('1234')
+  });
+
+  test('should not show email error message', () => {
+    const emailError = screen.queryByTestId('form-input-email-error');
+    expect(emailError).not.toBeInTheDocument();
+  });
+
+  test('should show email error message on invalid email', () => {
+    typeIntoForm({email: 'testgmail.com'})
+
+    findSubmitBtnAndClick('form-submit-btn')
+    
+    const emailError = screen.queryByTestId('form-input-email-error');
+    expect(emailError).toBeInTheDocument();
+  });
+
+  test('should not show password error message', () => {
+    expect(screen.queryByTestId('form-input-password-error')).not.toBeInTheDocument();
+  });
+
+  test('should show password error message on weak password', () => {
+    typeIntoForm({password: '1234'});
+    
+    findSubmitBtnAndClick('form-submit-btn')
+    
+    expect(screen.getByTestId('form-input-password-error')).toBeInTheDocument();
+  });
+
+  test('should not show confirm password error message', () => {
+    const confrimPasswordError = screen.queryByTestId('form-input-confirm-password-error');
+    expect(confrimPasswordError).not.toBeInTheDocument();
+  });
+
+  test('should show confirm password error message if password does not match', () => {
+    const {passwordEl, confirmPasswordEl} = typeIntoForm({
+      password: '1234', 
+      confirmPassword: '1232'
+    });
+
+    expect(passwordEl.value !== confirmPasswordEl.value).toBeTruthy();
+
+    findSubmitBtnAndClick('form-submit-btn')
+    expect(screen.getByTestId('form-input-confirm-password-error')).toBeInTheDocument();
+  });
+
+  test('should not show error messages if data is valid', () => {
+    typeIntoForm({
+      email: 'adonis@gmail.com',
+      password: 'XUG0gxf0ztx!rea3vhe',
+      confirmPassword: 'XUG0gxf0ztx!rea3vhe'
+    });
+
+    findSubmitBtnAndClick('form-submit-btn')
+    expect(screen.queryByTestId('form-input-email-error')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('form-input-password-error')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('form-input-confirm-password-error')).not.toBeInTheDocument();
+  })
+
 })
