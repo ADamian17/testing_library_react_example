8a4447aa69f4d922bbfec414a4bcc06cf821f5aa
ADamian17 <adonisdesuvikdo@gmail.com>
2022-03-12 20:41:38 -0800

    added helper function

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,48 @@ import userEvents from '@testing-library/user-event';
 
 import App from './App';
 
-test('input should be initially epmty', () => {
+beforeEach(() => {
   /* {render()} this method renders the component that we want to test, in the shadow dom and all it children  */
-  render(<App />)
 
+  // eslint-disable-next-line testing-library/no-render-in-setup
+  render(<App />)
+});
+
+const typeIntoForm = ({email, password, confirmPassword }) => {
+  const emailEl = screen.getByTestId('form-input-email');
+  const passwordEl = screen.getByTestId('form-input-password');
+  const confirmPasswordEl = screen.getByTestId('form-input-confirm-password');
+
+  if(email) {
+    userEvents.type(emailEl, email);
+  }
+  if(password) {
+    userEvents.type(passwordEl, password);
+  }
+  if(confirmPassword) {
+    userEvents.type(confirmPasswordEl, confirmPassword);
+  }
+
+  return {
+    emailEl,
+    passwordEl,
+    confirmPasswordEl
+  }
+}
+
+// afterEach(() => {
+//   console.log("this hook runs after each test");
+// });
+
+// beforeAll(() => {
+//   console.log("this hook runs once before all test");
+// });
+
+// afterAll(() => {
+//   console.log("this hook runs once after all test");
+// });
+
+test('input should be initially epmty', () => {
   /* {screen} help us to find the element*/
   const email = screen.getByTestId('form-input-email');
   const password = screen.getByTestId('form-input-password');
@@ -18,30 +56,17 @@ test('input should be initially epmty', () => {
 });
 
 test('should be able to type an email', () => {
-  /* {render()} this method renders the component that we want to test, in the shadow dom and all it children  */
-  render(<App />)
-
-  /* {screen} help us to find the element*/
-  const email = screen.getByTestId('form-input-email');
-  
-  userEvents.type(email, 'test@gmail.com');
-  expect(email.value).toBe('test@gmail.com')
+  const {emailEl} = typeIntoForm({email: 'test@gmail.com'})
+  expect(emailEl.value).toBe('test@gmail.com')
 });
 
 test('should be able to type a password', () => {
-  /* {render()} this method renders the component that we want to test, in the shadow dom and all it children  */
-  render(<App />)
-
-  /* {screen} help us to find the element*/
-  const password = screen.getByTestId('form-input-password');
-  
-  userEvents.type(password, '1234');
-  expect(password.value).toBe('1234')
+  const {passwordEl} = typeIntoForm({password: '1234'})
+  expect(passwordEl.value).toBe('1234')
 });
 
 test('should be able to type a confirm password', () => {
   /* {render()} this method renders the component that we want to test, in the shadow dom and all it children  */
-  render(<App />)
 
   /* {screen} help us to find the element*/
   const confirmPassword = screen.getByTestId('form-input-confirm-password');
@@ -51,15 +76,11 @@ test('should be able to type a confirm password', () => {
 });
 
 test('should not show email error message', () => {
-  render(<App />)
-
   const emailError = screen.queryByTestId('form-input-email-error');
   expect(emailError).not.toBeInTheDocument();
 });
 
 test('should show email error message on invalid email', () => {
-  render(<App />)
-
   const email = screen.getByTestId('form-input-email');
   userEvents.type(email, 'testgmail.com');
   
@@ -71,15 +92,11 @@ test('should show email error message on invalid email', () => {
 });
 
 test('should not show password error message', () => {
-  render(<App />)
-
   const passwordError = screen.queryByTestId('form-input-password-error');
   expect(passwordError).not.toBeInTheDocument();
 });
 
 test('should show password error message on weak password', () => {
-  render(<App />)
-
   const password = screen.getByTestId('form-input-password');
   userEvents.type(password, '1234');
   
@@ -91,15 +108,11 @@ test('should show password error message on weak password', () => {
 });
 
 test('should not show confirm password error message', () => {
-  render(<App />)
-
   const confrimPasswordError = screen.queryByTestId('form-input-confirm-password-error');
   expect(confrimPasswordError).not.toBeInTheDocument();
 });
 
 test('should show confirm password error message if password does not match', () => {
-  render(<App />)
-
   const password = screen.getByTestId('form-input-password');
   userEvents.type(password, '1234');
 
