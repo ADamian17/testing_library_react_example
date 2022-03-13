c1f56b616278a684035ace86d71e20569913a7c2
ADamian17 <adonisdesuvikdo@gmail.com>
2022-03-12 16:09:49 -0800

    added 10 test

--- src/App.js
+++ src/App.js
@@ -1,35 +1,120 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+import validator from 'validator';
+
 import './App.css';
-import { Container, Form } from 'react-bootstrap';
 
 function App() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+  const [emailError, setEmailError] = useState(null); 
+  const [passwordError, setPasswordError] = useState(null);
+  const [confirmPasswordError, setConfirmPasswordError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(!validator.isEmail(email)) {
+      setEmailError(`${email === '' ? 'Email can not be epmty' : 'Email is invalid'}`)
+    } else {
+      setEmail('')
+    }
+
+    if(!validator.isStrongPassword(password)) {
+      setPasswordError(`${ password === '' ? 'Password can not be epmty' : 'Password is to weak'}`)
+    } else {
+      setPassword('')
+    }
+
+    if(password !== confirmPassword) {
+      setConfirmPasswordError('Passwword does not match')
+    } else {
+      setConfirmPassword('')
+    }
+  }
+
+  // zgk.reg6ajq!RUB!drj
+
   return (
-    <Container className='my-5'>
+    <Container className='my-5 shadow p-5'>
      <Form>
        <Form.Group className='mb-3'>
          <Form.Label>Email Address</Form.Label>
          <Form.Control 
-          type='email' 
+          type='email'
+          value={email}
+          onChange={(e) => {
+            if (emailError) setEmailError('');
+            return setEmail(e.target.value)
+          }} 
           placeholder='Enter email' 
           data-testid='form-input-email' />
+          {
+            emailError && (
+            <Form.Text 
+              className="text-danger"
+              data-testid='form-input-email-error'>
+              {emailError}
+            </Form.Text>
+            )
+          }
        </Form.Group>
 
        <Form.Group className='mb-3'>
          <Form.Label>Email Address</Form.Label>
          <Form.Control 
-          type='password' 
+          type='password'
+          value={password}
+          onChange={(e) => {
+            if (passwordError) setPasswordError('');
+            return setPassword(e.target.value)
+          }}
           placeholder='Enter password' 
           data-testid='form-input-password' />
+          {
+            passwordError && (
+            <Form.Text 
+              className="text-danger"
+              data-testid='form-input-password-error'>
+              {passwordError}
+            </Form.Text>
+            )
+          }
        </Form.Group>
 
        <Form.Group className='mb-3'>
          <Form.Label>Email Address</Form.Label>
          <Form.Control 
-          type='password' 
+          type='password'
+          value={confirmPassword}
+          onChange={(e) => {
+            if (confirmPasswordError) setConfirmPasswordError(''); 
+            return setConfirmPassword(e.target.value)
+          }} 
           placeholder='Confirm password' 
           data-testid='form-input-confirm-password' />
+          {
+            confirmPasswordError && (
+            <Form.Text 
+              className="text-danger"
+              data-testid='form-input-confirm-password-error'>
+              {confirmPasswordError}
+            </Form.Text>
+            )
+          }
        </Form.Group>
+
+        <Button
+          onClick={handleSubmit} 
+          variant='primary' 
+          type='submit'
+          data-testid='form-submit-btn'>
+          Submit
+        </Button>
      </Form>
+
     </Container>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,6 @@
 import { render, screen } from '@testing-library/react';
+import userEvents from '@testing-library/user-event';
+
 import App from './App';
 
 test('input should be initially epmty', () => {
@@ -13,4 +15,102 @@ test('input should be initially epmty', () => {
   expect(email.value).toBe('')
   expect(password.value).toBe('')
   expect(confirmPassword.value).toBe('')
-})
+});
+
+test('should be able to type an email', () => {
+  /* {render()} this method renders the component that we want to test, in the shadow dom and all it children  */
+  render(<App />)
+
+  /* {screen} help us to find the element*/
+  const email = screen.getByTestId('form-input-email');
+  
+  userEvents.type(email, 'test@gmail.com');
+  expect(email.value).toBe('test@gmail.com')
+});
+
+test('should be able to type a password', () => {
+  /* {render()} this method renders the component that we want to test, in the shadow dom and all it children  */
+  render(<App />)
+
+  /* {screen} help us to find the element*/
+  const password = screen.getByTestId('form-input-password');
+  
+  userEvents.type(password, '1234');
+  expect(password.value).toBe('1234')
+});
+
+test('should be able to type a confirm password', () => {
+  /* {render()} this method renders the component that we want to test, in the shadow dom and all it children  */
+  render(<App />)
+
+  /* {screen} help us to find the element*/
+  const confirmPassword = screen.getByTestId('form-input-confirm-password');
+  
+  userEvents.type(confirmPassword, '1234');
+  expect(confirmPassword.value).toBe('1234')
+});
+
+test('should not show email error message', () => {
+  render(<App />)
+
+  const emailError = screen.queryByTestId('form-input-email-error');
+  expect(emailError).not.toBeInTheDocument();
+});
+
+test('should show email error message on invalid email', () => {
+  render(<App />)
+
+  const email = screen.getByTestId('form-input-email');
+  userEvents.type(email, 'testgmail.com');
+  
+  const submitBtn = screen.getByTestId('form-submit-btn');
+  userEvents.click(submitBtn)
+  
+  const emailError = screen.queryByTestId('form-input-email-error');
+  expect(emailError).toBeInTheDocument();
+});
+
+test('should not show password error message', () => {
+  render(<App />)
+
+  const passwordError = screen.queryByTestId('form-input-password-error');
+  expect(passwordError).not.toBeInTheDocument();
+});
+
+test('should show password error message on weak password', () => {
+  render(<App />)
+
+  const password = screen.getByTestId('form-input-password');
+  userEvents.type(password, '1234');
+  
+  const submitBtn = screen.getByTestId('form-submit-btn');
+  userEvents.click(submitBtn)
+  
+  const passwordError = screen.queryByTestId('form-input-password-error');
+  expect(passwordError).toBeInTheDocument();
+});
+
+test('should not show confirm password error message', () => {
+  render(<App />)
+
+  const confrimPasswordError = screen.queryByTestId('form-input-confirm-password-error');
+  expect(confrimPasswordError).not.toBeInTheDocument();
+});
+
+test('should show confirm password error message if password does not match', () => {
+  render(<App />)
+
+  const password = screen.getByTestId('form-input-password');
+  userEvents.type(password, '1234');
+
+  const confirmPassword = screen.getByTestId('form-input-confirm-password');
+  userEvents.type(confirmPassword, '1232');
+
+  expect(password !== confirmPassword).toBeTruthy();
+
+  const submitBtn = screen.getByTestId('form-submit-btn');
+  userEvents.click(submitBtn)
+
+  const confrimPasswordError = screen.queryByTestId('form-input-confirm-password-error');
+  expect(confrimPasswordError).toBeInTheDocument();
+});
